0c229543ed6b4b967be7206abe6863b321bd00ae
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2022-09-21 09:14:44 +0100

    Output footer nav by default

--- src/themes/$tghp:lowerCaseHyphenated$/footer.php
+++ src/themes/$tghp:lowerCaseHyphenated$/footer.php
@@ -1,4 +1,14 @@
     <footer class="site-footer">
+        <nav class="site-footer__nav">
+            <?php
+            wp_nav_menu([
+                'theme_location' => 'footer-nav',
+                'container' => false,
+                'menu_class' => 'site-nav'
+            ]);
+            ?>
+        </nav>
+
         <div class="site-footer__copyright">
             <?= sprintf('&copy; %s %s', date('Y'), get_bloginfo('name')) ?>
         </div>
