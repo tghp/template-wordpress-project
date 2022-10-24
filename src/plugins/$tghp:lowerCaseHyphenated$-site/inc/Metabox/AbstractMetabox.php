83f7d13d4c5a2f33b6e1f1d42a53424c21a515e1
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2022-10-24 18:19:14 +0100

    Added templated WYSIWYG options

--- src/plugins/$tghp:lowerCaseHyphenated$-site/inc/Metabox/AbstractMetabox.php
+++ src/plugins/$tghp:lowerCaseHyphenated$-site/inc/Metabox/AbstractMetabox.php
@@ -7,4 +7,13 @@ use TGHP\$tghp:classCase$\Abstract$tghp:classCase$;
 abstract class AbstractMetabox extends Abstract$tghp:classCase$
 {
 
+    public function getStrippedTinymceConfig($extra = [])
+    {
+        return [
+            'toolbar1' => 'bold,italic,link,' . $extra['toolbar1'] ?? '',
+            'toolbar2' => $extra['toolbar2'] ?? '',
+            'toolbar3' => $extra['toolbar3'] ?? '',
+        ];
+    }
+
 }
